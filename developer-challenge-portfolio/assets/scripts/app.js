fd610118eecf45b5ed16579f7dadfe212e33c02d
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-05-26 13:07:59 -0400

    Updated the about me section

--- developer-challenge-portfolio/assets/scripts/app.js
+++ developer-challenge-portfolio/assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "enjoy the challenges of learning how to code."
+				"am a Full-Stack Web Developer.", "enjoy the challenges of learning how to code.", "also love maps, books, and dogs!"
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
